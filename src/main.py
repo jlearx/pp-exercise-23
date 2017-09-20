aa377930175be9fa0e99aad41b2dbdb25be49cb7
U-AMR\jlearx <jlearx@jlearx-MOBL1.amr.corp.intel.com>
2017-09-20 13:34:49 -0700

    Implemented solution to the exercise

--- src/main.py
+++ src/main.py
@@ -12,14 +12,32 @@ def readFile(filename):
         
         while line:
             line = line.rstrip('\n')
-            CountItem(nameDict, line)
+            num = int(line)
+            numList.append(num)
             line = open_file.readline()   
+    
+    return numList
+
+def getOverlapSets(list1, list2):
+    set1 = set(list1)
+    set2 = set(list2)
+    
+    overlap = list(set1 & set2)
+    
+    return overlap
 
 if __name__ == '__main__':
     happyFile = "../happynumbers.txt"
     primeFile = "../primenumbers.txt"
-    happyNums = []
-    primeNums = []
-    overlap = []
     
-     
+    happyNums = readFile(happyFile)
+    primeNums = readFile(primeFile)
+    overlap = getOverlapSets(happyNums, primeNums)
+    
+    print("The Happy Numbers are:")
+    print(happyNums)
+    print("The Prime Numbers are:")
+    print(primeNums)
+    print("Overlap between the two sets of numbers:")
+    print(overlap)
+    
